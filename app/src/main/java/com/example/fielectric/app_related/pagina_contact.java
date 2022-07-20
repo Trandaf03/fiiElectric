d2fdfbc8a3f059eca1c5d23e2283c8f4ff2ac3a5
Trandafir Andrei <trandafgaming@gmail.com>
2022-07-20 21:34:58 +0300

    Updated

--- app/src/main/java/com/example/fielectric/app_related/pagina_contact.java
+++ app/src/main/java/com/example/fielectric/app_related/pagina_contact.java
@@ -12,8 +12,10 @@ import com.example.fielectric.R;
 public final class pagina_contact extends AppCompatActivity {
 
     private TextView text;
-    private TextView imagineInsta;
-    private TextView imagineFacebook;
+    private TextView imagineInstaTrandaf;
+    private TextView imagineFacebookTrandaf;
+    private TextView imagineInstaRares;
+    private TextView imagineFacebookRares;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +23,34 @@ public final class pagina_contact extends AppCompatActivity {
         setContentView(R.layout.activity_pagina_contact);
 
         text = (TextView) findViewById(R.id.textDescriere);
-        imagineInsta = (TextView) findViewById(R.id.instaText);
-        imagineFacebook = (TextView) findViewById(R.id.textFb);
 
-        text.setText("Echipa fiiElectric! momentan se rezumă doar la un membru, eu. Mă numesc Andrei" +
-                " și sunt un mare pasionat de mașini electrice, mai bine zis deorice ce este electric" +
-                " precum biciclete sau trotinete electrice. Vreau un viitor verde, așa că m-am decis " +
-                "să ajut lumea în a-și alege un autovehicul electric, fie el mașină, bicicletă, trotinetă " +
+        imagineInstaTrandaf = (TextView) findViewById(R.id.instaText);
+        imagineFacebookTrandaf = (TextView) findViewById(R.id.textFb);
+        imagineFacebookRares = (TextView) findViewById(R.id.textFbrares);
+        imagineInstaRares = (TextView) findViewById(R.id.instaTextrares);
+
+        text.setText("Echipa fiiElectric! momentan se rezumă doar la doi membri, Trandafir Andrei și Nica Rareș. Mă numesc Andrei" +
+                " și suntem mari pasionat de mașini electrice, mai bine zis deorice ce este electric" +
+                " precum biciclete sau trotinete electrice. Vrem un viitor verde, așa că ne-am decis " +
+                "să ajutăm lumea în a-și alege un autovehicul electric, fie el mașină, bicicletă, trotinetă " +
                 "sau motocicletă.");
+
         String facebook = "<a href=\"https://www.facebook.com/trandaf10\">Trandfir Andrei</a>";
         String insta = "<a href=\"https://www.instagram.com/trandaf10/\">Trandf10</a>";
 
-        imagineInsta.setMovementMethod(LinkMovementMethod.getInstance());
-        imagineFacebook.setMovementMethod(LinkMovementMethod.getInstance());
+        String facebookrares = "<a href=\"https://www.facebook.com/daniel.nica.712\">Nica Rareș</a>";
+        String instarares = "<a href=\"https://www.instagram.com/raresh.23/\">raresh.23</a>";
+
+        imagineInstaTrandaf.setMovementMethod(LinkMovementMethod.getInstance());
+        imagineFacebookTrandaf.setMovementMethod(LinkMovementMethod.getInstance());
+        imagineInstaRares.setMovementMethod(LinkMovementMethod.getInstance());
+        imagineFacebookRares.setMovementMethod(LinkMovementMethod.getInstance());
 
 
-        imagineInsta.setText(Html.fromHtml(insta));
-        imagineFacebook.setText(Html.fromHtml(facebook));
+        imagineInstaTrandaf.setText(Html.fromHtml(insta));
+        imagineFacebookTrandaf.setText(Html.fromHtml(facebook));
+        imagineInstaRares.setText(Html.fromHtml(instarares));
+        imagineFacebookRares.setText(Html.fromHtml(facebookrares));
 
     }
 
